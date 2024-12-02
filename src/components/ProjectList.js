0471a995065e03dcab1dee95fe8a8d7cf96ce7a2
Dallin Johnson <13dallin13@gmail.com>
2024-12-02 16:53:56 -0700

    UI adjustments for project list element

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -13,10 +13,24 @@ const ProjectList = () => {
   const createElement = () => {
     const container = document.createElement("div");
     container.classList.add("project-list");
+
+    const header = document.createElement("h3");
+    header.textContent = "Active Projects";
+
+    const newProjectBtn = document.createElement("button");
+    newProjectBtn.textContent = "+ New Project";
+
+    const controls = document.createElement("div");
+    controls.classList.add("task-list-controls");
+    controls.appendChild(header);
+    controls.appendChild(newProjectBtn);
+
+    container.appendChild(header);
     projects.map((project) => {
       const projectItem = ProjectItem(project);
       container.appendChild(projectItem.getElement());
     });
+    container.appendChild(newProjectBtn);
     return container;
   };
 
