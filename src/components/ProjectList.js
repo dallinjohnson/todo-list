fbd8cb96a872fa46ba15dc12333912ec71cddf6b
Dallin Johnson <13dallin13@gmail.com>
2024-12-04 23:18:11 -0700

    Remove excess UI elements from project list element

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -14,23 +14,10 @@ const ProjectList = () => {
     const container = document.createElement("div");
     container.classList.add("project-list");
 
-    const header = document.createElement("h3");
-    header.textContent = "Active Projects";
-
-    const newProjectBtn = document.createElement("button");
-    newProjectBtn.textContent = "+ New Project";
-
-    const controls = document.createElement("div");
-    controls.classList.add("task-list-controls");
-    controls.appendChild(header);
-    controls.appendChild(newProjectBtn);
-
-    container.appendChild(header);
     projects.map((project) => {
       const projectItem = ProjectItem(project);
       container.appendChild(projectItem.getElement());
     });
-    container.appendChild(newProjectBtn);
     return container;
   };
 
